1badae15c76c5073ef3121449e1755ac748d3c8a
MrBotCipher <32300516+RAJANAGORI@users.noreply.github.com>
2021-05-26 12:03:58 +0530

    Changes done for py2

--- install.sh
+++ install.sh
@@ -26,5 +26,12 @@ cat script.list | xargs go get -u
 GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
 nuclei -update-templates
 
+#installing pip2 module and frida-tool
+wget -L https://bootstrap.pypa.io/pip/2.7/get-pip.py -O get-pip.py -P /tmp/
+python /tmp/get-pip.py
+python -m pip install --upgrade pip setuptools wheel
+sudo python -m pip install frida-tools
+sudo pip3 install frida-tools
+
 #creating symlink for the above tools
 sudo ln -s ~/go/bin/* /usr/local/bin/
