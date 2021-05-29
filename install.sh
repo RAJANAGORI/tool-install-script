0aedaf766d60c01eeaff25c306069e87e35b364c
MrBotCipher <32300516+RAJANAGORI@users.noreply.github.com>
2021-05-29 21:09:30 +0530

    Bugs are resolves

--- install.sh
+++ install.sh
@@ -9,20 +9,16 @@ echo "Installing required tools"
 
 # #Installing Go language 
 echo "Installing GO Language"
-mkdir -p ~/.go
 wget -c https://golang.org/dl/go1.16.4.linux-amd64.tar.gz -P /tmp/
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf /tmp/go1.16.4.linux-amd64.tar.gz
 
-export GOPATH='~/.go'
-
-export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+export PATH=$PATH:/usr/local/go/bin:~/go/bin >> ~/.zshrc
 
 sudo apt-get update && \
 sudo apt-get upgrade -y && \
 cat tool.list | xargs sudo apt-get install -y 
 
 #installing go tool
-
 cat script.list | xargs go get -u 
 
 #Installing Nuclei
@@ -32,7 +28,7 @@ nuclei -update-templates
 #installing pip2 module and frida-tool
 cd /tmp/
 wget -L https://bootstrap.pypa.io/pip/2.7/get-pip.py -O get-pip.py
-python get-pip.py
+python /tmp/get-pip.py
 python -m pip install --upgrade pip setuptools wheel
 python -m pip install frida-tools
 sudo pip3 install frida-tools
