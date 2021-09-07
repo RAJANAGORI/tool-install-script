4333af8aab99291d427ea691b280afca2b7c1855
Raja Nagori <rajanagori19@gmail.com>
2021-09-07 05:08:43 -0400

    Changes done with version detection condition

--- install.sh
+++ install.sh
@@ -1,39 +1,76 @@
 #!/bin/bash
-#Author= Abhinav Saxena
-#Contributor= Raja Nagori
+#Script-Designer : Raja Nagori
 
 bgreen='\033[1;32m'
 printf "\n\n${bgreen}***********************************************************************\n"
 printf "${bgreen} GoldenEye Installation Script ${reset}\n\n"
+
+#update and upgrade
+echo "Update and Upgrade the required dependecies for security updates"
+apt-get update &&\
+apt-get upgrade -y
+
+sleep 5s
+
 echo "Installing required tools"
 
-# #Installing Go language 
-echo "Installing GO Language"
-wget -c https://golang.org/dl/go1.16.4.linux-amd64.tar.gz -P /tmp/
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf /tmp/go1.16.4.linux-amd64.tar.gz
+#install snap 
+apt-get install snapd &&\
+    systemctl enable --now snapd apparmor
 
-export PATH=$PATH:/usr/local/go/bin:~/go/bin >> ~/.zshrc
+#checking for git version
+git_version=$(git --version)
+if [ $git_version ]; then
+    echo "Git is install" + $git_version
+else
+    echo "not installed? I'm going to fire the command in 5 second"
+    sleep 5s
+    apt-get install -y git
+fi
 
-sudo apt-get update && \
-sudo apt-get upgrade -y && \
-cat tool.list | xargs sudo apt-get install -y 
+#Checking python version
+echo "checking for the python version"
+ver=$(python3 -c"import sys; print(sys.version_info.major)")
+if [ $ver -eq 3 ]; then
+    echo "python version is 3"
+    sleep 5s
+    cat tool.list | xargs sudo apt-get install -y
+    #installing dirsearch 
+    git clone https://github.com/maurosoria/dirsearch.git ~/home/${USER} &&\
+    pip3 install -r ~/home/${USER}/dirsearch/requirements.txt
+else 
+    echo "Unknown python version: $ver"
+    exit 1
+fi
 
-#installing go tool
-cat script.list | xargs go get -u 
+#checking for go version
+go_version=$(go version)
+if [$go_version]; then
+    echo "GO is install with the version" + $go_version
+    
+    #installing go tool
+    cat script.list | xargs go get -u 
+    
+    #Installing Nuclei
+    GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+    nuclei -update-templates
+else
+    echo "not installed? I'm going to fire the command in 5 second"
+    sleep 5s
+    snap install --classic --channel=1.15/stable go
+ 
+    #installing go tool
+    cat script.list | xargs go get -u 
+    
+    #Installing Nuclei
+    GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+    nuclei -update-templates
+fi
 
-#Installing Nuclei
-GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
-nuclei -update-templates
-
-#installing pip2 module and frida-tool
-cd /tmp/
-wget -L https://bootstrap.pypa.io/pip/2.7/get-pip.py -O get-pip.py
-python /tmp/get-pip.py
-python -m pip install --upgrade pip setuptools wheel
-python -m pip install frida-tools
-sudo apt-get install python3-pip
-sudo pip3 install frida-tools
-export PATH="/home/kali/.local/bin:$PATH"
+#install findomain
+cat emumeration-script | xargs wget -P /tmp &&\
+chmod +x /tmp/findomain-linux &&\
+mv /tmp/findomain-linux /usr/bin/
 
 #creating symlink for the above tools
-sudo ln -s ~/go/bin/* /usr/local/bin/
+ln -s ~/go/bin/* /usr/local/bin/
---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+command -v git >/dev/null 2>&1 ||
+  apt-get install git
+}33335555
